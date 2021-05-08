0cc70c481f9afc033ff98f16012ef95e619e2a10
CAROLINA MARÍA CAMACHO ZAMBRANO <36022220+ccamacho5237@users.noreply.github.com>
2021-05-07 19:39:18 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(10000)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.clearScreen()
+})
